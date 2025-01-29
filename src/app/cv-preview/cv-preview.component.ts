0105a54289fa0b7f55d44255e7876f1cc4d2a7eb
MafioP <mario.pereda.08@gmail.com>
2025-01-29 13:54:32 +0100

    Added german and english versions

--- src/app/cv-preview/cv-preview.component.ts
+++ src/app/cv-preview/cv-preview.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { CvService } from '../services/cv.service';
 import jsPDF from 'jspdf';
+import { ImageConverterService } from '../services/image-converter.service';
+import { ResumeData, ContactDetails } from '../interfaces/profile';
+import * as enData from '../../assets/content-en.json';
+import * as deData from '../../assets/content-de.json';
 
 @Component({
   selector: 'app-cv-preview',
@@ -8,93 +12,191 @@ import jsPDF from 'jspdf';
   styleUrls: ['./cv-preview.component.css'],
 })
 export class CvPreviewComponent {
+  profileData = [deData,enData];
+  profilePicBase64: string | null = null;
   skills: string[] = [];
-  experiences = [
-    {
-      position: 'Amazon Delivery Driver',
-      company: 'PRB Logistik GmbH, Rammstein Germany',
-      date: 'October 2024 - December 2024',
-      details: [
-        'Delivered parcels to various locations throughout the delivery area.',
-        'Employed this time to improve my German language skills while communicating with customers.',
-      ],
-    },
-    {
-      position: 'Intern Software Engineer',
-      company: 'Comlet GmbH, Zweibrucken Germany',
-      date: 'March 2024 - August 2024',
-      details: [
-        'ERASMUS exchange program for bachelor thesis.',
-        'Worked with embedded devices, including the electronics and software side of them, developing a tool for test automation.',
-        'Learned the benefits of CI/CD while developing the project.',
-      ],
-    },
-    {
-      position: 'Intern Software Engineer',
-      company: 'Evenbytes, Santander Spain',
-      date: 'February 2023 - August 2023',
-      details: [
-        'Full-stack web developer with Angular and NodeJS, working in Google Cloud Platform.',
-        'Developed a Gmail addon for automated invoice processing and data collection.',
-        'Developed a proof of concept system for managing access control devices.',
-      ],
-    },
-  ];
-  languages: string[] = [
-    'Spanish - Native',
-    'English - Advanced',
-    'German - Beginner (A1)',
-  ];
-  projects = [
-    {
-      title: 'CAN and OBD reader for BMW',
-      description:
-        'A HW and SW solution to read, analyze, and display parameters from the CAN bus and the Onboard Diagnostics Port.',
-    },
-    {
-      title: 'Factory builder Unity game',
-      description:
-        'A simple Unity engine game for building automated production lines.',
-    },
-    {
-      title: 'Dynamic CV Geneator',
-      description:
-        'A tool to optimize the tailoring of CVs to specific job offers, by providing a fast way of updating the user most relevant skills'
-    }
-  ];
 
   constructor(private cvService: CvService) {
     this.cvService.cvData.subscribe((data) => {
       this.skills = data.skills;
     });
+    this.imageConverterService
+      .convertImageToBase64('assets/Profilepic.png')
+      .then(
+        (base64) => {
+          this.profilePicBase64 = base64;
+        },
+        (error) => {
+          console.error('Error converting image:', error);
+        }
+      );
   }
 
-  downloadPDF(): void {
-    const element = document.getElementById('cv-template'); // Target the CV container
+  async downloadPDF() {
+    const doc = new jsPDF({
+      unit: 'px',
+      format: 'a4',
+      orientation: 'portrait',
+    });
 
-    if (element) {
-      element.style.fontSize = '14px'; // Adjust the size as needed
+    let offsetY = 10; // Starting Y position
+    const marginX = 15;
 
-      const doc = new jsPDF({
-        unit: 'px',
-        format: 'a4',
-        orientation: 'portrait',
+    // Add horizontal list
+    const addHorizontalList = (
+      items: string[],
+      startX: number,
+      startY: number
+    ): number => {
+      const itemHeight = 10; // Height for each item
+      const padding = 10; // Space between items
+      const rectRoundess = 5;
+      let currentX = startX + padding / 2;
+      console.log("Adding skills");
+      items.forEach((item) => {
+        doc.setFillColor(100, 100, 100);
+        doc.setTextColor(255, 255, 255);
+        doc.setFontSize(12);
+        // Draw box
+        doc.roundedRect(
+          currentX - padding / 2,
+          startY - padding,
+          doc.getTextWidth(item) + padding,
+          itemHeight + padding / 2,
+          rectRoundess,
+          rectRoundess,
+          'F'
+        );
+        // Add text centered inside the box
+        doc.text(item, currentX, startY); // Slight padding for alignment
+        currentX += doc.getTextWidth(item) + padding * 1.5; // Move to the next position
+      });
+      doc.setTextColor(0, 0, 0);
+
+      return startY + itemHeight + 10; // Return the new Y position after the list
+    };
+
+    const setHeader = (text: String) => {
+      doc.setFont('times', 'bold');
+      doc.setFontSize(16);
+      doc.text(text as string, marginX, offsetY);
+      offsetY += 7;
+      doc.setDrawColor('#ddd');
+      doc.line(
+        marginX,
+        offsetY,
+        doc.internal.pageSize.width - marginX,
+        offsetY
+      );
+      offsetY += 13;
+      doc.setFont('times', 'normal');
+      doc.setFontSize(12);
+    };
+
+    const setContactInfo = (profileData: ContactDetails) => {
+      console.log("Setting contact info ", profileData);
+      doc.addImage(
+        this.profilePicBase64 as string,
+        'PNG',
+        marginX,
+        offsetY,
+        80,
+        80
+      ); // (x, y, width, height)
+      offsetY += 20;
+      const tempXOffset = doc.internal.pageSize.getWidth() / 2;
+      // Set Font and Styles
+      doc.setFont('times', 'bold');
+      doc.setFontSize(18);
+      doc.text(profileData.name, tempXOffset, (offsetY += 10), {
+        align: 'center',
       });
 
-      const margin = 0; // Pixels
-      const pageWidth = doc.internal.pageSize.getWidth();
-
-      doc.html(element, {
-        x: margin,
-        y: margin,
-        width: pageWidth - margin * 2,
-        windowWidth: element.scrollWidth,
-        callback: () => {
-          // Restore original font size after rendering
-          element.style.fontSize = '';
-          doc.save('Generated-CV.pdf');
-        },
+      doc.setFontSize(14);
+      doc.setFont('times', 'italic');
+      doc.text(profileData.title, tempXOffset, (offsetY += 10), {
+        align: 'center',
       });
-    }
+
+      doc.setFont('times', 'normal');
+      doc.setFontSize(12);
+
+      // Contact Info
+      doc.text(profileData.phoneNumber, tempXOffset, (offsetY += 10), {
+        align: 'center',
+      });
+      doc.text(profileData.gmailAddress, tempXOffset, (offsetY += 10), {
+        align: 'center',
+      });
+      doc.text(profileData.livingAddress, tempXOffset, (offsetY += 10), {
+        align: 'center',
+      });
+
+      // Hyperlinks (LinkedIn & GitHub)
+      doc.setTextColor(0, 0, 255); // Blue color to indicate a link
+      doc.textWithLink(
+        'LinkedIn',
+        tempXOffset - doc.getTextWidth('LinkedIn') - 10,
+        (offsetY += 10),
+        { url: 'https://www.linkedin.com/in/mario-pereda-puyo-57061b253/' }
+      );
+
+      doc.textWithLink('GitHub', tempXOffset + 10, offsetY, {
+        url: 'https://github.com/MafioP',
+      });
+    };
+
+    const sections = ['contact', 'cv-template-1', 'skills', 'cv-template-2'];
+
+      for (const [langIndex, langData] of this.profileData.entries()) {
+        for (const [index, id] of sections.entries()) {
+          if (id === 'contact') {
+            setContactInfo(langData.contactDetails);
+          }
+          if (id === 'skills') {
+            console.log("[SKILLS (before addpage)]Current page" + doc.getCurrentPageInfo().pageNumber);
+            doc.addPage();
+            console.log("[SKILLS (after addpage)]Current page" + doc.getCurrentPageInfo().pageNumber);
+            offsetY = 20;
+            setHeader('Skills');
+            offsetY = addHorizontalList(this.skills, marginX, offsetY);
+            offsetY = addHorizontalList(langData.softSkills, marginX, offsetY);
+            console.log('Added Skills', id, 'offset', offsetY);
+            setHeader('Languages');
+            offsetY = addHorizontalList(langData.languages, marginX, offsetY);
+            console.log('Added Language', id, 'offset', offsetY);
+            offsetY += doc.internal.pageSize.getHeight() - 15;
+          }
+
+          const element = document.getElementsByClassName(id).item(langIndex) as HTMLElement;
+          if (element) {
+            console.log('Offset before html:', offsetY, 'i:', index);
+            console.log("HTML: ",element);
+            console.log("[ELEMENT]Current page" + doc.getCurrentPageInfo().pageNumber);
+
+            // Wait for doc.html() to finish before proceeding
+            await new Promise<void>((resolve) => {
+              doc.html(element, {
+                x: marginX,
+                y: offsetY + doc.internal.pageSize.getHeight() * (langIndex*2),
+                html2canvas: { scale: 0.55 },
+                callback: () => {
+                  console.log('Starting offset:', offsetY, 'i:', index);
+                  offsetY += element.offsetHeight / 2;
+                  console.log('Added', id, 'offset', offsetY);
+                  resolve(); // Ensure the loop waits for this step
+                },
+              });
+            });
+          }
+        }
+        if (langIndex < this.profileData.length-1){
+          doc.addPage();
+          offsetY = 20;
+          console.log("New page from loop end");
+        }
+      }
+    // Save the PDF after processing all sections
+    doc.save('dynamic-multiple-sections.pdf');
   }
 }
